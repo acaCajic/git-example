db33df6a95fcdaa33982065ee33e4265d947a7fa
acacajic <acacajic@gmail.com>
2022-04-15 15:49:31 +0200

    feat(KJ-523): new commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var numbers = [1, 2, 3, 4, 5, 6];
 var array = [];
 
+const button = document.getElementsByClassName(".btn");
+const countParagraph = document.getElementsByClassName(".counter");
+
+let counter = 0;
+
+button.addEventListener("click", () => {
+  console.log(counter++);
+  counter++;
+  countParagraph.innerHTML = counter;
+});
+
 numbers.forEach((number) => {
   if (number > 2) {
     console.log(number);
