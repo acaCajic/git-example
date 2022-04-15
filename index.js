f6f9657a62f4d39974a9da691b92126de8c7c39c
acacajic <acacajic@gmail.com>
2022-04-15 16:08:16 +0200

    feat(develop): new array added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var numbers = [1, 2, 3, 4, 5, 6];
 var array = [];
+var secondArray = [1, 2, 3, 4, 5];
 
 numbers.forEach((number) => {
   if (number > 2) {
     console.log(number);
   }
 });
+console.log("new version");
