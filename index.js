20da3749f50caf1a869fa9edc604fd886d7316c4
acacajic <acacajic@gmail.com>
2022-04-15 15:58:34 +0200

    fix(develop): update develop with new function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var numbers = [1, 2, 3];
+var numbers = [1, 2, 3, 4, 5, 6];
 var array = [];
 
 numbers.forEach((number) => {
   if (number > 2) {
-    array.push(number);
+    console.log(number);
   }
 });
